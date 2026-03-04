b91a221ef246097ae859677b449afaf0e3c50270
hyperFounder <ribeiro.ryan@protonmail.com>
2026-03-04 15:44:05 +0000

    initial commit

--- src/main/java/org/financial/consumer/TransactionProcessor.java
+++ src/main/java/org/financial/consumer/TransactionProcessor.java
@@ -15,8 +15,6 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-
-
 public class TransactionProcessor{
 
     private static final Logger log = LoggerFactory.getLogger(TransactionProcessor.class);
